acbc579d6f45b60caf020a534517a9ce2f742edd
Makodzeba <makodzebahtihory@gmail.com>
2024-03-08 22:15:25 +0200

    Fixed code structure and performance

---
+++ helper_ltj3_140919_j6bj.jsx
@@ -0,0 +1,5 @@
+orange - true
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+class MyClass { constructor() { this.property = getRandomString(); } }
